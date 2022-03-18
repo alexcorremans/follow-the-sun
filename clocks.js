165ace9fe33b99fee7588ce4a0f44ebfc0696a08
Alexandra Corremans <alexandra.corremans@gmail.com>
2022-03-18 14:27:01 +1100

    fix spacing

--- clocks.js
+++ clocks.js
@@ -39,29 +39,29 @@ var DateTime = luxon.DateTime;
 
 // helpers
 function generateDT(tzString) {
-    return DateTime.now().setLocale('en-GB').setZone(tzString);
+  return DateTime.now().setLocale('en-GB').setZone(tzString);
 }
 
 function getClass(hour) {
-    if (hour >=6 && hour < 8) {
-        return "dawn";
-    } else if (hour >=8 && hour < 10) {
-        return "morning";
-    } else if (hour >=10 && hour < 12) {
-        return "late-morning";
-    } else if (hour >=12 && hour < 14) {
-        return "lunch";
-    } else if (hour >=14 && hour < 16) {
-        return "afternoon";
-    } else if (hour >=16 && hour < 18) {
-        return "late-afternoon";
-    } else if (hour >=18 && hour < 20) {
-        return "sunset";
-    } else if (hour >=20 && hour < 22) {
-        return "evening";
-    } else {
-        return "night";
-    }
+  if (hour >=6 && hour < 8) {
+      return "dawn";
+  } else if (hour >=8 && hour < 10) {
+      return "morning";
+  } else if (hour >=10 && hour < 12) {
+      return "late-morning";
+  } else if (hour >=12 && hour < 14) {
+      return "lunch";
+  } else if (hour >=14 && hour < 16) {
+      return "afternoon";
+  } else if (hour >=16 && hour < 18) {
+      return "late-afternoon";
+  } else if (hour >=18 && hour < 20) {
+      return "sunset";
+  } else if (hour >=20 && hour < 22) {
+      return "evening";
+  } else {
+      return "night";
+  }
 }
 
 function generateHTML(i, name, team) {
@@ -111,14 +111,14 @@ function displayClock(i, timezone) {
 
 // main function, runs every second
 function displayClocks() {
-    for (var i = 0; i < numberOfTimezones; ++i) {
-      // first time it runs, create HTML structure
-      if (document.querySelector('main').childElementCount < numberOfTimezones) {
-        generateHTML(i, timeZones[i].name, timeZones[i].team);
-      }
-      // display dynamic data
-      displayClock(i, timeZones[i].timezone)
+  for (var i = 0; i < numberOfTimezones; ++i) {
+    // first time it runs, create HTML structure
+    if (document.querySelector('main').childElementCount < numberOfTimezones) {
+      generateHTML(i, timeZones[i].name, timeZones[i].team);
     }
+    // display dynamic data
+    displayClock(i, timeZones[i].timezone)
+  }
 }
 
 window.onload = displayClocks();
